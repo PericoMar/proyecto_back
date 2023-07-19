9a42cefa369adcfe1c09d365c990468a450df0bf
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2023-07-19 19:40:53 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require("http") /* module.exports de HTTP que es una libreria de NodeJS que necesitamos
+                            Usamos la función require para poder meterla en 
+                            la constante (const) http (objeto)*/
+// console.log({module})
+// module.exports = "Exporto lo que quiero"
+
+function requestController() {
+    console.log("Recibimos una request");
+}
+//Cofigurar nuestro servidor    
+const server = http.createServer(requestController) // Cada vez que llega una request a nuestro servidor
+                                                    // createServer ejecuta nuestra funcion requestController
+
+server.listen(4000) // Nuestro servidor debe de "escuchar" un determinado puerto (lo pasamos por parametros)
+//Para probar hacemos una peticion de red a nuestra propia maquina (localhost) y con el puerto 4000 que es el
+//que hemos puesto localhost:4000
+
+
+
+
+
+
+
+
